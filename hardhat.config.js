7e68d687ff7440697aebc1eee711f8bea24fdc59
ahmetenesdur <ahmetenesdur@gmail.com>
2023-06-22 01:31:44 +0300

    Testing Contracts Done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
 // Import the dotenv library to read environment variables from the .env.local file
 require("dotenv").config({
   path: "./.env.local",
@@ -9,8 +8,9 @@ require("dotenv").config({
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
+    testnet: {
       url: process.env.RPC_URL,
+      chainId: 97,
       accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
     },
   },
